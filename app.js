a16ab2c2985587616e7ec60510abdc6ec56057bf
Yevgen Kulik <kxamed@gmail.com>
2017-07-09 00:44:01 +0300

    add zec

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ app.use(express_logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make miner data accessible to the router
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     req.json = {
-        "title"       : config.title,
-        "animation"   : config.animation,
-        "header"      : config.header ? config.header : config.title,
-        "miners"      : miners.json,
-        "refresh"     : config.web_refresh,
-        "tolerance"   : config.tolerance,
-        "temperature" : config.temperature,
-        "hashrates"   : config.hashrates,
-        "updated"     : moment().format("YYYY-MM-DD HH:mm:ss")
+        "title": config.title,
+        "animation": config.animation,
+        "header": config.header ? config.header : config.title,
+        "miners": miners.json,
+        "refresh": config.web_refresh,
+        "tolerance": config.tolerance,
+        "temperature": config.temperature,
+        "hashrates": config.hashrates,
+        "updated": moment().format("YYYY-MM-DD HH:mm:ss")
     };
     next();
 });
@@ -37,7 +37,7 @@ app.use('/', routes);
 app.use('/miners', miners);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -47,7 +47,7 @@ app.use(function(req, res, next) {
 
 // Development error handler will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -57,7 +57,7 @@ if (app.get('env') === 'development') {
 }
 
 // Production error handler, no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -90,7 +90,7 @@ miners.json = [];
 
 logger.info('config: ' + config.miners.length + ' rig(s) configured');
 
-config.miners.forEach(function(item, i, arr) {
+config.miners.forEach(function (item, i, arr) {
     logger.trace(item.name + ': config[' + i + ']');
 
     // settings
@@ -101,6 +101,7 @@ config.miners.forEach(function(item, i, arr) {
     m.name = c.name;
     m.host = c.host;
     m.port = c.port;
+    m.miner= c.miner;
     m.poll = (typeof c.poll !== 'undefined') ? c.poll : config.miner_poll;
     m.timeout = (typeof c.timeout !== 'undefined') ? c.timeout : config.miner_timeout;
 
@@ -116,104 +117,205 @@ config.miners.forEach(function(item, i, arr) {
     c.last_seen = null;
     c.last_good = null;
 
-    // socket
-    m.socket = new net.Socket()
+    if (m.miner == "claymore") {
+        // socket
+        m.socket = new net.Socket()
 
-    .on('connect', function() {
-        logger.info(m.name + ': connected to ' + m.socket.remoteAddress + ':' + m.socket.remotePort);
-        var req = '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}';
-        ++m.reqCnt;
-        logger.trace(m.name + ': req[' + m.reqCnt + ']: ' + req);
-        m.socket.write(req + '\n');
-        m.socket.setTimeout(m.timeout);
-    })
+            .on('connect', function () {
+                logger.info(m.name + ': connected to ' + m.socket.remoteAddress + ':' + m.socket.remotePort);
+                var req = '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}';
+                ++m.reqCnt;
+                logger.trace(m.name + ': req[' + m.reqCnt + ']: ' + req);
+                m.socket.write(req + '\n');
+                m.socket.setTimeout(m.timeout);
+            })
 
-    .on('timeout', function() {
-        logger.warn(m.name + ': response timeout');
-        m.socket.destroy();
-        miners.json[i] = {
-            "name"       : m.name,
-            "host"       : hostname(),
-            "uptime"     : "",
-            "eth"        : "",
-            "dcr"        : "",
-            "eth_hr"     : "",
-            "dcr_hr"     : "",
-            "temps"      : "",
-            "pools"      : "",
-            "ver"        : "",
-            "target_eth" : "",
-            "target_dcr" : "",
-            "comments"   : c.comments,
-            "offline"    : c.offline,
-            "warning"    : null,
-            "error"      : 'Error: no response',
-            "last_seen"  : c.last_seen ? c.last_seen : 'never'
-        };
-    })
+            .on('timeout', function () {
+                logger.warn(m.name + ': response timeout');
+                m.socket.destroy();
+                miners.json[i] = {
+                    "name": m.name,
+                    "host": hostname(),
+                    "uptime": "",
+                    "eth": "",
+                    "dcr": "",
+                    "eth_hr": "",
+                    "dcr_hr": "",
+                    "temps": "",
+                    "pools": "",
+                    "ver": "",
+                    "target_eth": "",
+                    "target_dcr": "",
+                    "comments": c.comments,
+                    "offline": c.offline,
+                    "warning": null,
+                    "error": 'Error: no response',
+                    "last_seen": c.last_seen ? c.last_seen : 'never'
+                };
+            })
 
-    .on('data', function(data) {
-        ++m.rspCnt;
-        logger.trace(m.name + ': rsp[' + m.rspCnt + ']: ' + data.toString().trim());
-        c.last_seen = moment().format("YYYY-MM-DD HH:mm:ss");
-        m.socket.setTimeout(0);
-        var d = JSON.parse(data);
-        miners.json[i] = {
-            "name"       : m.name,
-            "host"       : hostname(),
-            "uptime"     : moment.duration(parseInt(d.result[1]), 'minutes').format('d [days,] hh:mm'),
-            "eth"        : d.result[2],
-            "dcr"        : d.result[4],
-            "eth_hr"     : d.result[3],
-            "dcr_hr"     : d.result[5],
-            "temps"      : d.result[6],
-            "pools"      : d.result[7],
-            "ver"        : d.result[0],
-            "target_eth" : c.target_eth,
-            "target_dcr" : c.target_dcr,
-            "comments"   : c.comments,
-            "offline"    : c.offline,
-            "ti"         : c.ti ? c.ti : null,
-            "error"      : null
-        };
-        if (c.target_eth && config.tolerance) {
-            if (miners.json[i].eth.split(';')[0] / 1000 < c.target_eth * (1 - config.tolerance / 100)) {
-                miners.json[i].warning = 'Low hashrate';
-                miners.json[i].last_good = c.last_good ? c.last_good : 'never';
-            } else {
-                miners.json[i].warning = null;
-                c.last_good = moment().format("YYYY-MM-DD HH:mm:ss");
-            }
-        }
-    })
+            .on('data', function (data) {
+                ++m.rspCnt;
+                logger.trace(m.name + ': rsp[' + m.rspCnt + ']: ' + data.toString().trim());
+                c.last_seen = moment().format("YYYY-MM-DD HH:mm:ss");
+                m.socket.setTimeout(0);
+                var d = JSON.parse(data);
+                miners.json[i] = {
+                    "name": m.name,
+                    "host": hostname(),
+                    "uptime": moment.duration(parseInt(d.result[1]), 'minutes').format('d [days,] hh:mm'),
+                    "eth": d.result[2],
+                    "dcr": d.result[4],
+                    "eth_hr": d.result[3],
+                    "dcr_hr": d.result[5],
+                    "temps": d.result[6],
+                    "pools": d.result[7],
+                    "ver": d.result[0],
+                    "target_eth": c.target_eth,
+                    "target_dcr": c.target_dcr,
+                    "comments": c.comments,
+                    "offline": c.offline,
+                    "ti": c.ti ? c.ti : null,
+                    "error": null
+                };
+                if (c.target_eth && config.tolerance) {
+                    if (miners.json[i].eth.split(';')[0] / 1000 < c.target_eth * (1 - config.tolerance / 100)) {
+                        miners.json[i].warning = 'Low hashrate';
+                        miners.json[i].last_good = c.last_good ? c.last_good : 'never';
+                    } else {
+                        miners.json[i].warning = null;
+                        c.last_good = moment().format("YYYY-MM-DD HH:mm:ss");
+                    }
+                }
+            })
 
-    .on('close', function() {
-        logger.info(m.name + ': connection closed');
-        setTimeout(poll, m.poll);
-    })
+            .on('close', function () {
+                logger.info(m.name + ': connection closed');
+                setTimeout(poll, m.poll);
+            })
 
-    .on('error', function(e) {
-        logger.error(m.name + ': socket error: ' + e.message);
-        miners.json[i] = {
-            "name"       : m.name,
-            "host"       : hostname(),
-            "uptime"     : "",
-            "eth"        : "",
-            "dcr"        : "",
-            "eth_hr"     : "",
-            "dcr_hr"     : "",
-            "temps"      : "",
-            "pools"      : "",
-            "ver"        : "",
-            "target_eth" : "",
-            "target_dcr" : "",
-            "comments"   : c.comments,
-            "offline"    : c.offline,
-            "warning"    : null,
-            "error"      : e.name + ': ' + e.message,
-            "last_seen"  : c.last_seen ? c.last_seen : 'never'
-        };
-    });
+            .on('error', function (e) {
+                logger.error(m.name + ': socket error: ' + e.message);
+                miners.json[i] = {
+                    "name": m.name,
+                    "host": hostname(),
+                    "uptime": "",
+                    "eth": "",
+                    "dcr": "",
+                    "eth_hr": "",
+                    "dcr_hr": "",
+                    "temps": "",
+                    "pools": "",
+                    "ver": "",
+                    "target_eth": "",
+                    "target_dcr": "",
+                    "comments": c.comments,
+                    "offline": c.offline,
+                    "warning": null,
+                    "error": e.name + ': ' + e.message,
+                    "last_seen": c.last_seen ? c.last_seen : 'never'
+                };
+            });
+    } else if (m.miner == "zec") {
+        // socket
+        m.socket = new net.Socket()
+
+            .on('connect', function () {
+                logger.info(m.name + ': connected to ' + m.socket.remoteAddress + ':' + m.socket.remotePort);
+                var req = '{"id":0,"jsonrpc":"2.0","method":"getstat"}';
+                ++m.reqCnt;
+                logger.trace(m.name + ': req[' + m.reqCnt + ']: ' + req);
+                m.socket.write(req + '\n');
+                m.socket.setTimeout(m.timeout);
+            })
+
+            .on('timeout', function () {
+                logger.warn(m.name + ': response timeout');
+                m.socket.destroy();
+                miners.json[i] = {
+                    "name": m.name,
+                    "host": hostname(),
+                    "uptime": "",
+                    "eth": "",
+                    "dcr": "",
+                    "eth_hr": "",
+                    "dcr_hr": "",
+                    "temps": "",
+                    "pools": "",
+                    "ver": "",
+                    "target_eth": "",
+                    "target_dcr": "",
+                    "comments": c.comments,
+                    "offline": c.offline,
+                    "warning": null,
+                    "error": 'Error: no response',
+                    "last_seen": c.last_seen ? c.last_seen : 'never'
+                };
+            })
+
+            .on('data', function (data) {
+                ++m.rspCnt;
+                logger.trace(m.name + ': rsp[' + m.rspCnt + ']: ' + data.toString().trim());
+                c.last_seen = moment().format("YYYY-MM-DD HH:mm:ss");
+                m.socket.setTimeout(0);
+                var d = JSON.parse(data);
+                miners.json[i] = {
+                    "name": m.name,
+                    "host": hostname(),
+                    "uptime": moment.duration(parseInt(d.result[1]), 'minutes').format('d [days,] hh:mm'),
+                    "eth": d.result[2],
+                    "dcr": d.result[4],
+                    "eth_hr": d.result[3],
+                    "dcr_hr": d.result[5],
+                    "temps": d.result[6],
+                    "pools": d.result[7],
+                    "ver": d.result[0],
+                    "target_eth": c.target_eth,
+                    "target_dcr": c.target_dcr,
+                    "comments": c.comments,
+                    "offline": c.offline,
+                    "ti": c.ti ? c.ti : null,
+                    "error": null
+                };
+                if (c.target_eth && config.tolerance) {
+                    if (miners.json[i].eth.split(';')[0] / 1000 < c.target_eth * (1 - config.tolerance / 100)) {
+                        miners.json[i].warning = 'Low hashrate';
+                        miners.json[i].last_good = c.last_good ? c.last_good : 'never';
+                    } else {
+                        miners.json[i].warning = null;
+                        c.last_good = moment().format("YYYY-MM-DD HH:mm:ss");
+                    }
+                }
+            })
+
+            .on('close', function () {
+                logger.info(m.name + ': connection closed');
+                setTimeout(poll, m.poll);
+            })
+
+            .on('error', function (e) {
+                logger.error(m.name + ': socket error: ' + e.message);
+                miners.json[i] = {
+                    "name": m.name,
+                    "host": hostname(),
+                    "uptime": "",
+                    "eth": "",
+                    "dcr": "",
+                    "eth_hr": "",
+                    "dcr_hr": "",
+                    "temps": "",
+                    "pools": "",
+                    "ver": "",
+                    "target_eth": "",
+                    "target_dcr": "",
+                    "comments": c.comments,
+                    "offline": c.offline,
+                    "warning": null,
+                    "error": e.name + ': ' + e.message,
+                    "last_seen": c.last_seen ? c.last_seen : 'never'
+                };
+            });
+    }
 
     function poll() {
         m.socket.connect(m.port, m.host);
@@ -223,21 +325,21 @@ config.miners.forEach(function(item, i, arr) {
         poll();
     } else {
         miners.json[i] = {
-            "name"       : m.name,
-            "host"       : hostname(),
-            "uptime"     : "",
-            "eth"        : "",
-            "dcr"        : "",
-            "eth_hr"     : "",
-            "dcr_hr"     : "",
-            "temps"      : "",
-            "pools"      : "",
-            "ver"        : "",
-            "target_eth" : "",
-            "target_dcr" : "",
-            "comments"   : c.comments,
-            "offline"    : c.offline,
-            "error"      : null
+            "name": m.name,
+            "host": hostname(),
+            "uptime": "",
+            "eth": "",
+            "dcr": "",
+            "eth_hr": "",
+            "dcr_hr": "",
+            "temps": "",
+            "pools": "",
+            "ver": "",
+            "target_eth": "",
+            "target_dcr": "",
+            "comments": c.comments,
+            "offline": c.offline,
+            "error": null
         };
     }
 });
